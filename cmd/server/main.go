7b8ef6e74a184e68c9892443a1aeabb1020475ef
Vlad <vladka@webdevelop.pro>
2023-06-19 18:46:35 +0100

    removed http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/webdevelop-pro/migration-service/internal/adapters"
 	"github.com/webdevelop-pro/migration-service/internal/adapters/repository/postgres"
 	"github.com/webdevelop-pro/migration-service/internal/app"
-	"github.com/webdevelop-pro/migration-service/internal/ports"
 	"github.com/webdevelop-pro/migration-service/internal/services"
 	"go.uber.org/fx"
 )
@@ -33,13 +32,11 @@ func main() {
 			app.New,
 			// Bind App with service interface
 			func(mig *app.App) services.Migration { return mig },
-			// Http Server
-			server.New,
 		),
 
 		fx.Invoke(
 			// InitHandlers
-			ports.InitHandlers,
+			// ports.InitHandlers,
 			// Run application
 			RunApp,
 		),
@@ -56,18 +53,14 @@ func main() {
 	log.Info().Msg("done")
 }
 
-func RunHttpServer(lc fx.Lifecycle, srv *server.HttpServer) {
-	server.StartServer(lc, srv)
-}
-
-func RunApp(sd fx.Shutdowner, _app *app.App, c *configurator.Configurator, lc fx.Lifecycle, srv *server.HttpServer) {
+func RunApp(sd fx.Shutdowner, _app *app.App, c *configurator.Configurator, lc fx.Lifecycle) {
 	init := flag.Bool("init", false, "initialize service by creating migration table at DB")
 	finalSql := flag.String("final-sql", "", "if provided - program return final SQL for migrations without applying it. Argument = service name")
 	force := flag.Bool("force", false, "force apply migration without version checking. Accept files or dir paths. Will not update service version if applied version is lower, then already applied")
 	skip := flag.Bool("fake", false, "fake do not apply any migration but mark according migrations in migration_services table as completed")
 	check := flag.Bool("check", false, "check verifies if all hashes of migrations are equal to those in migration table. If no - returns list of files with migrations, that have differences. Can accept files or dirs of migrations as arguments")
 	checkApply := flag.Bool("check-apply", false, "check-apply compares hashes of all migrations with hashes in DB and try to apply those, that have differences. Can accept files or dirs of migrations as arguments")
-	applyOnly := flag.Bool("apply-only", false, "apply and shutdown migration service, do not start web service")
+	// applyOnly := flag.Bool("apply-only", false, "apply and shutdown migration service, do not start web service")
 
 	flag.Parse()
 
@@ -101,15 +94,7 @@ func RunApp(sd fx.Shutdowner, _app *app.App, c *configurator.Configurator, lc fx
 	}
 
 	RunMigrations(sd, _app, c)
-
-	if *applyOnly == false {
-		// Run server
-		RunHttpServer(lc, srv)
-	} else {
-		sd.Shutdown()
-	}
-	// Run server
-	RunHttpServer(lc, srv)
+	sd.Shutdown()
 }
 
 func RunMigrations(sd fx.Shutdowner, _app *app.App, c *configurator.Configurator) {
